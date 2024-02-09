43b59b83d8283235811e967335ab563bfc15aa20
kushalkhadgi <117558911+kushalkhadgi@users.noreply.github.com>
2024-02-09 23:43:44 +0530

    day 10.cpp

---
+++ day 10.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+/*
+int main()
+{
+    int number = 5;
+    for(int i = 1; i<=number; i++)
+    {
+        for(int j=0; j<i; j++)
+        {
+            cout<<i+j;
+        }
+        cout<<endl;
+    }
+}
+*/
+
+int main()
+{
+    int number = 10;
+    for(int i = 1; i<=number; i++)
+    {
+        for(int j = 1; j<=i; j++)
+        {
+            if((i+j)%2==0)
+            {
+                cout<<"1";
+            }
+            else
+            {
+                cout<<"0";
+            }
+        }
+        cout<<endl;
+    }
+}
