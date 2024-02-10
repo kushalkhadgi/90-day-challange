24b5156e372893f1f6fbca74535f1d77648fdb51
kushalkhadgi <117558911+kushalkhadgi@users.noreply.github.com>
2024-02-10 23:59:38 +0530

    day 11.cpp

--- day 11.cpp
+++ day 11.cpp
@@ -1,1 +1,26 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int number = 10;
+    for(int i=0; i<number; i++)
+    {
+        for(int j=0; j<(number-i); j++)
+        {
+            cout<<"*";
+        }
+        
+        for(int z=0; z<i; z++)
+        {
+            cout<<"  ";
+        }
+        
+        for(int j=0; j<(number-i); j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
