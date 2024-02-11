c3a83f5c98b43294e173ecb076d8cb4f7c20aa62
kushalkhadgi <117558911+kushalkhadgi@users.noreply.github.com>
2024-02-11 23:51:27 +0530

    day 12.cpp

---
+++ day 12.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 10;
+	
+	int i, j, k = n;
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			if (j >= k)
+				cout << "* ";
+			else
+				cout << " ";
+		}
+		k--;
+		cout << "\n";
+  }   
+	return 0;
+}
